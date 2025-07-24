6ab3652a487e8bb62c5288ad8e02f0c7438ec7f8
Alex Kozik <alex.kozik@yahoo.com>
2025-07-24 10:35:33 -0700

    Zoom and location

--- src/components/WorldMap.tsx
+++ src/components/WorldMap.tsx
@@ -77,7 +77,7 @@ const WorldMap: React.FC<WorldMapProps> = ({
   useEffect(() => {
     if (!mapRef.current || leafletMapRef.current) return;
 
-    const map = L.map(mapRef.current).setView([51.505, -0.09], 13);
+    const map = L.map(mapRef.current).setView([39.8283, -98.5795], 6);
 
     L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
